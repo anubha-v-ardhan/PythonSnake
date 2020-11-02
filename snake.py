e9308b78d0d3b4f65822ea9a5d25ea664b9aa014
anubha-v-ardhan <71918957+anubha-v-ardhan@users.noreply.github.com>
2020-11-02 08:28:03 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,42 +1,35 @@
-# import curses module
 import curses
-
-# import the randint function from the random module
 from random import randint
 
-# setup window
-curses.initscr()                    # initialize screen
-win = curses.newwin(20, 60, 0, 0)   # initialize new window
-win.keypad(1)                       # switch primary input method to keypad
-curses.noecho()                     # do not repeat each input
-curses.curs_set(0)                  # do not show cursor
-win.border(0)                       # don't show default border
-win.nodelay(1) # -1                 # there should be no delay    
+curses.initscr()                    
+win = curses.newwin(20, 60, 0, 0)   
+win.keypad(1)                       
+curses.noecho()                     
+curses.curs_set(0)                  
+win.border(0)                      
+win.nodelay(1) # -1                  
 
-# snake and food
-snake = [(4, 10), (4, 9), (4, 8)]   # snake's starting coordinates
-food = (10, 20)                     # food's starting coordinates
+snake = [(4, 10), (4, 9), (4, 8)]   
+food = (10, 20)                     
 
-win.addch(food[0], food[1], '#')    # add character to depict the food
+win.addch(food[0], food[1], '#')   
 
-# maing game logic
-score = 0                           # initialize score to 0
+score = 0                           
     
-ESC = 27                            # ESC key has ASCII value of 27
-key = curses.KEY_RIGHT              # store ASCII value of right key
+ESC = 27                            
+key = curses.KEY_RIGHT              
 win.addch(food[0], food[1], 'O')    
-while key != ESC:                   # run the game until ESC is pressed
+while key != ESC:                   
     win.addstr(0, 2, 'Score ' + str(score) + ' ')
-    win.timeout(100)                # game runs on 100 milliseconds per frame
+    win.timeout(100)               
 
     prev_key = key                  
     event = win.getch()
-    key = event if event != -1 else prev_key    # handle key input
+    key = event if event != -1 else prev_key    
 
     if key not in [curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, ESC]:
-        key = prev_key                          # for every key that we want to read
+        key = prev_key                          
 
-    # calculate the next coordinates
     y = snake[0][0]
     x = snake[0][1]
     if key == curses.KEY_DOWN:
@@ -48,18 +41,15 @@ while key != ESC:                   # run the game until ESC is pressed
     if key == curses.KEY_RIGHT:
         x += 1
 
-    snake.insert(0, (y, x))                     # add new body part for snake when it eats the food
+    snake.insert(0, (y, x))                     
 
-    # check if we hit the border
     if y == 0: break
     if y == 19: break
     if x == 0: break
     if x == 59: break
 
-    # if snake runs over itself
     if snake[0] in snake[1:]: break
 
-    # if snake runs over the food
     if snake[0] == food:
         # eat the food
         score += 1
@@ -70,7 +60,6 @@ while key != ESC:                   # run the game until ESC is pressed
                 food = ()
         win.addch(food[0], food[1], '#')
     else:
-        # move snake
         last = snake.pop()
         win.addch(last[0], last[1], ' ')
     
